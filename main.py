7401b86e9c32eedcfcffece64f9245c8410c5fc5
nolotbublika <161640172+nolotbublika@users.noreply.github.com>
2024-04-03 09:03:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class Building:
+    def __init__(self):
+        self.numberOfFloors = int
+        self.buildingType = str
+
+    def __eq__(self, other):
+
+        return self. numberOfFloors == other.buildingType
+
